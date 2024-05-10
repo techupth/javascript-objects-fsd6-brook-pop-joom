44789971f6e5402cf4e9c5339b2d11f6810f0cdc
Pop <shounen.pop@gmail.com>
2024-05-10 14:38:51 +0700

    fix: ex2

--- ex-2.js
+++ ex-2.js
@@ -12,6 +12,8 @@ let socialMediaShare = "facebookShare";
 product.ratings = 4.5;
 product.photo =
   "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+product.code = "IT0101010";
+product.price = 6000;
 product[socialMediaShare] = 45.5;
 
 console.log(product);
